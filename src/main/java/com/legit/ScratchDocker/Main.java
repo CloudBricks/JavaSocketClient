110672e3112a053b337f9bf8e2b0d0608661c627
kirbyquerby <nathanxyzdias@gmail.com>
2017-08-05 00:35:43 -0500

    Add run configuration

--- src/main/java/com/legit/ScratchDocker/Main.java
+++ src/main/java/com/legit/ScratchDocker/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         startSocketServer();
+        System.out.println("Socket server running");
     }
 
     static void startSocketServer() {
